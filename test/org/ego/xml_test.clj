26d1f54680837f5583bd08b3959f48910c9783f4
Andrew Stein <texodusmedia@gmail.com>
2009-08-02 04:20:35 -0400

    test fixes

--- test/org/ego/xml_test.clj
+++ test/org/ego/xml_test.clj
@@ -3,16 +3,26 @@
         [clojure.contrib.test-is]))
  
 (deftest test-parse
-  (testing "parse"
+  (testing "parse - basic interleave"
     (is (nil? (process :connect "test")))
     (is (empty? (process :upstream "test" "<te")))
     (is (empty? (process :upstream "test" "st")))
     (is (empty? (process :upstream "test" "s")))
     (is (nil? (process :connect "test2")))
+    (is (empty? (process :upstream "test2" "<te")))
     (is (= (first (process :upstream "test" ">"))
            {:state nil, :tag :start-element, :qname "tests", :attrs []}))
-    (is (= [{:state nil, :tag :start-element, :qname "test", :attrs []} 
-            {:state nil, :tag :end-element, :qname "test", :attrs []}]
-           (process :upstream "test2" "<test></test>")))))
-
+    (is (= (process :upstream "test2" "st></test>")
+           [{:state nil, :tag :start-element, :qname "test", :attrs []} 
+            {:state nil, :tag :end-element, :qname "test", :attrs []}]))
+    (is (nil? (process :disconnect "test")))
+    (is (nil? (process :disconnect "test2"))))
+  (testing "parse - attributes"
+    (is (nil? (process :connect "test")))
+    (is (= (first (process :upstream "test" "<test name='x'>"))
+           {:state nil, :tag :start-element, :qname "test", :attrs [["name" "x"]]}))
+    (is (nil? (process :connect "test2")))
+    (is (= (first (process :upstream "test2" "</test name='x' val=\"value\">"))
+           {:state nil, :tag :end-element, :qname "test", :attrs [["val" "value"] ["name" "x"]]}))))
+           
 
--- test/org/ego_test.clj
+++ test/org/ego_test.clj
@@ -2,8 +2,5 @@
   (:require [org.ego.stanza-test]
             [org.ego.xml-test])
   (:use [clojure.contrib.test-is]))
-        
-(deftest test-common
-  (testing "alter-nil"))
    
 (run-tests 'org.ego.stanza-test 'org.ego.xml-test)
