78506fb88c2aca33f4b1fe42cecdb4f235faabc9
Andrew Stein <texodusmedia@gmail.com>
2009-08-02 04:36:22 -0400

    more tests

--- test/org/ego/stanza_test.clj
+++ test/org/ego/stanza_test.clj
@@ -6,4 +6,11 @@
   (testing "alter-nil"
     (let [temp-ref (ref {:a 1})]
       (is (= (dosync (alter-nil temp-ref assoc :a 2)) nil))
-      (is (= (:a @temp-ref))))))
+      (is (= (:a @temp-ref)))))
+  (testing "process - simple interleave"
+    (is (nil? (process :connect "test")))
+    (is (empty? (process :upstream "test" {:state nil, :tag :start-element, :qname "test", :attrs []})))
+    (is (= (process :upstream "test" {:state nil, :tag :end-element, :qname "test", :attrs []})
+           [{:tag :test, :attrs {}, :content nil}]))
+    (is (nil? (process :disconnect "test")))))
+
